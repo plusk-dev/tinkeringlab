8b7e120a4e65b3ee415253ebd9f1eec758513b7d
saivdakoder <satvicdhawan14@gmail.com>
2024-02-10 02:06:22 +0530

    toasting +bugs resolved

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -2,22 +2,38 @@ import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 import * as React from 'react';
 import { Button } from "./components/ui/button";
-import logo from 'assets/logo.svg'
+import logo from './assets/logo.svg'
+import { Toaster,toast } from "sonner"
+
 
 
 export default function Login() {
   return <>
+  
   <div className="flex h-screen flex-col">
+
       <div className="flex justify-center items-end h-1/2 bg-zinc-800 text-center">
         <img src={logo} className="h-2/3"/>
       </div>
       <div className="flex h-1/2 justify-center  bg-zinc-800" >
+       
         <div className="flex flex-col text-center pt-10">
+        <Toaster className="bg-black"/>  
           <GoogleLogin 
+              
                 onSuccess={credentialResponse => {
                   if (credentialResponse.credential) {
-                    let info = jwtDecode(credentialResponse.credential);
-                    console.log(info);
+                    const details:any=jwtDecode(credentialResponse.credential);
+                    let info:string[]=details.email.split('@');
+                    if(info[1]==="iitjammu.ac.in"){
+                      toast("Logged in successfully.")
+                    }
+                    else{
+                      toast("Uh-oh",{
+                        description:"Please login with the valid email id"
+                      })
+                     
+                    }
                   }
                 }}
                 onError={() => {
