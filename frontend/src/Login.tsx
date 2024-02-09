b19f810894371ac30c851cfb732b4bd5e510fa7d
plusk-dev <yuvrajmotiramani5115@gmail.com>
2024-02-09 23:47:11 +0530

    sm sm login page

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -2,30 +2,29 @@ import { GoogleLogin } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 import * as React from 'react';
 import { Button } from "./components/ui/button";
-import logo from 'assets/logo.svg'
+import logo from './assets/logo.svg'
 
 
 export default function Login() {
   return <>
-  <div className="flex h-screen flex-col">
-      <div className="flex justify-center items-end h-1/2 bg-zinc-800 text-center">
-        <img src={logo} className="h-2/3"/>
+    <div className="flex h-screen flex-col bg-zinc-950">
+      <div className="flex justify-center items-end h-1/2 text-center">
+        <img src={logo} className="h-2/5" />
       </div>
-      <div className="flex h-1/2 justify-center  bg-zinc-800" >
-        <div className="flex flex-col text-center pt-10">
-          <GoogleLogin 
-                onSuccess={credentialResponse => {
-                  if (credentialResponse.credential) {
-                    let info = jwtDecode(credentialResponse.credential);
-                    console.log(info);
-                  }
-                }}
-                onError={() => {
-                  console.log('Login Failed');
-                }}
-              />
-            <br></br>
-            <Button >View as guest</Button>
+      <div className="flex h-1/2 justify-center  bg-zinc-950" >
+        <div className="flex flex-col text-center pt-0">
+          <GoogleLogin
+            onSuccess={credentialResponse => {
+              if (credentialResponse.credential) {
+                let info = jwtDecode(credentialResponse.credential);
+                console.log(info);
+              }
+            }}
+            onError={() => {
+              console.log('Login Failed');
+            }}
+          />
+          <Button className="mt-3" variant="secondary">View as guest</Button>
         </div>
       </div>
     </div>
