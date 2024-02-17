d5bc2cbde91c475d02219c4c292f25d3031f2598
plusk-dev <yuvrajmotiramani5115@gmail.com>
2024-02-17 17:38:43 +0530

    added a form

--- frontend/src/Booking.tsx
+++ frontend/src/Booking.tsx
@@ -1,15 +1,90 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/ui/Navbar";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns"
+import { Calendar as CalendarIcon } from "lucide-react"
 
-
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover"
+import { Textarea } from "@/components/ui/textarea"
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectLabel,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select"
 export default function Booking() {
+	const [date, setDate] = React.useState<Date>()
+	const [isSmall, setisSmall] = React.useState(false);
+	window.addEventListener("resize", (_) => {
+		setisSmall(window.innerWidth <= 1000);
+	})
+	React.useEffect(() => {
+		setisSmall(window.innerWidth <= 1000);
+	}, [])
 	return (
 		<div className="h-screen parent">
 			<Navbar />
-			<div className="p-4 pt-6 h-5/6">
-
+			<div className="container pt-8">
+				<h1 className="text-3xl">Issue a component</h1>
+				<br />
+				<div className={isSmall ? "" : "flex gap-2"}>
+					<div className=" w-full">
+						Select a component
+						<Select>
+							<SelectTrigger>
+								<SelectValue placeholder="Select a component" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+									<SelectLabel>Fruits</SelectLabel>
+									<SelectItem value="apple">Apple</SelectItem>
+									<SelectItem value="banana">Banana</SelectItem>
+									<SelectItem value="blueberry">Blueberry</SelectItem>
+									<SelectItem value="grapes">Grapes</SelectItem>
+									<SelectItem value="pineapple">Pineapple</SelectItem>
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+					</div>
+					<div className="w-1/2">
+						Date of return <br />
+						<Popover>
+							<PopoverTrigger asChild>
+								<Button
+									variant={"outline"}
+									className={cn(
+										"w-full justify-start text-left font-normal",
+										!date && "text-muted-foreground"
+									)}
+								>
+									<CalendarIcon className="mr-2 h-4 w-4" />
+									{date ? format(date, "PPP") : <span>Pick a date</span>}
+								</Button>
+							</PopoverTrigger>
+							<PopoverContent className="w-full p-0">
+								<Calendar
+									mode="single"
+									selected={date}
+									onSelect={setDate}
+									initialFocus
+								/>
+							</PopoverContent>
+						</Popover>
+					</div>
+				</div>
+				Purpose of issue:
+				<Textarea placeholder="Purpose of issue..." className="mb-2" />
+				<Button className="w-full">Submit</Button>
 			</div>
 		</div>
 	)
--- frontend/src/components/ui/Navbar.tsx
+++ frontend/src/components/ui/Navbar.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Button } from "./button";
 import { GiHamburgerMenu } from "react-icons/gi";
-
 export default function Navbar() {
 	const [isSmall, setisSmall] = useState(false);
 	const [hidden, setHidden] = useState(true);
@@ -17,14 +16,15 @@ export default function Navbar() {
 		<div className={isSmall ? "flex flex-col gap-3 border-b-2  border-zinc-400 content-normal bg-zinc-200 navbar" : "flex gap-3 border-b-2  border-zinc-400 content-normal bg-zinc-200 navbar"}>
 			<div className="flex justify-between">
 				<Link to="/dashboard" className="p-3 hover:rounded-lg">
-					Tinkering Lab Logo here
+					Idhar logo hoga
 				</Link>
 				<Button variant="ghost" className={!isSmall ? "hidden" : ""} onClick={() => setHidden(!hidden)}><GiHamburgerMenu /></Button>
 			</div>
 			<div className={isSmall ? "flex-col flex hidden" + (!hidden ? "hidden" : "") : "flex"}>
 				<Link to="/login" className="p-3 hover:bg-zinc-300 hover:rounded-lg">My Bookings</Link>
-				<Link to="/dashboard/booking" className="p-3 hover:bg-zinc-300 hover:rounded-lg">New Booking</Link>
-				<Link to="/login" className="p-3 hover:bg-zinc-300 hover:rounded-lg">Inventory</Link>
+				<Link to="/dashboard/booking" className="p-3 hover:bg-zinc-300 hover:rounded-lg">Book a machine</Link>
+				<Link to="/login" className="p-3 hover:bg-zinc-300 hover:rounded-lg">Issue a component</Link>
+				<Link to="/login" className="p-3 hover:bg-zinc-300 hover:rounded-lg">Book a workstation</Link>
 			</div>
 		</div>
 	</>
