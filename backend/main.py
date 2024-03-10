3f6ced4bc06e6fd92caf975a70b00d353630b77b
plusk-dev <yuvrajmotiramani5115@gmail.com>
2024-03-10 13:24:01 +0530

    nothig much

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from models import session, Admin, User
 from fastapi.responses import JSONResponse
-from routes import bookings_router
+from routes import bookings_router, inventory_router
 from utils import get_credential, object_as_dict
 import re
 import jwt
@@ -12,6 +12,7 @@ email_regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 JWT_SECRET = get_credential("JWT_SECRET")
 app = FastAPI()
 app.mount("/bookings", bookings_router)
+app.mount("/inventory", inventory_router)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
--- backend/routes/inventory.py
+++ backend/routes/inventory.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Depends
+from models import Component, session
+from utils import object_as_dict
 
 inventory_router = FastAPI()
 
+
 @inventory_router.get("/")
-async def index():
-    return {}
+async def get_all_components():
+    return {
+        "components": [object_as_dict(component) for component in session.query(Component).all()]
+    }
