df3c091dc4f98c2d6e217cf55c4428492a0937f6
plusk-dev <yuvrajmotiramani5115@gmail.com>
2024-03-25 13:55:24 +0530

    added cbart

--- frontend/src/Dashboards/adminDash.tsx
+++ frontend/src/Dashboards/adminDash.tsx
@@ -17,36 +17,31 @@ export default function AdminDashboard() {
   }, [])
   ChartJS.register(ArcElement, Tooltip, Legend);
   const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Red', 'Green', 'Yellow'],
     datasets: [
       {
         label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
+        data: [12, 5, 7],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
+          'rgba(252, 186, 3, 0.2)'
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
           'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
+          'rgba(252, 186, 3, 1)'
         ],
         borderWidth: 1,
       },
     ],
   };
+
   return (
     <>
       <div className={`hover:rounded-lg ${isSmall ? "" : "flex"} max-h-screen`}>
         <Sidebar />
-        <div className="flex flex-col w-screen ps-1 pe-1 max-h-screen overflow-auto">
+        <div className="flex flex-col w-screen ps-1 pe-1 overflow-hidden" style={{ maxHeight: '98vh' }}>
           <div className="flex m-1">
             <Card className="w-full flex info-card">
               <div className="diamond-shape m-5 h-14 w-14"></div>
@@ -58,8 +53,20 @@ export default function AdminDashboard() {
           <div className={isSmall ? "" : "flex-1 flex h-full mb-1"}>
             <ReqHandler />
             <Card className="h-full info-card m-1 flex-1 p-4">
-              <div className={isSmall ? "" : "h-full flex flex-col overflow-y-scroll"}>
+              <div className={isSmall ? "" : "flex flex-col overflow-y-scroll h-5/6"}>
                 <div className="flex-1 flex flex-col gap-1">
+                  <CardTitle>Request success rate</CardTitle>
+                  <div className="flex">
+                    <Card className="flex-1 m-1 p-4 info-card">
+                      <CardTitle className="text-xl">Last Month</CardTitle>
+                      <Doughnut data={data} />
+                    </Card>
+                    <Card className="flex-1 m-1 p-4 info-card">
+                      <CardTitle className="text-xl">All Time</CardTitle>
+                      <Doughnut data={data} />
+                    </Card>
+                  </div>
+
                   <CardTitle>Components</CardTitle>
                   <div className="flex flex-1">
                     <Card className="flex-1 m-1 info-card p-4">
@@ -99,17 +106,6 @@ export default function AdminDashboard() {
                       <span className="text-4xl">3/</span><span>10</span>
                     </Card>
                   </div>
-                  <CardTitle>Stats</CardTitle>
-                  <div className="flex flex-1">
-                    <Card className="flex-1 m-1 info-card p-4">
-                      <Doughnut data={data} />
-                    </Card>
-                    <Card className="flex-1 m-1 p-4">
-                      <Doughnut data={data} />
-
-                    </Card>
-                  </div>
-
                 </div>
               </div>
             </Card>
