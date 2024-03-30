9b5e09d17c7e300bf09b4d6dcc3677c9fd629261
plusk-dev <yuvrajmotiramani5115@gmail.com>
2024-03-30 18:17:59 +0530

    cool glasses

--- frontend/src/requests/Req.tsx
+++ frontend/src/requests/Req.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Cog } from "lucide-react";
+import { Clock, Puzzle, Glasses } from "lucide-react";
 import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 
 interface Data {
@@ -19,18 +19,17 @@ interface ReqProps {
 }
 
 const ICONS = {
-  'Component': "",
-  'Machine': "",
-  'Intern': ""
+  'Component': <Puzzle />,
+  'Machine': <Clock />,
+  'Intern': <Glasses />
 }
 
 const Req: React.FC<ReqProps> = ({ data, updateStatus }) => {
-
   return (
     <>
-      <Card className={`mt-1 ${data.status==="resolved"?"bg-pink-400":"bg-blue-200"} border-blue-700	border-[1px] border-dashed`}>
+      <Card className={`mt-1 ${data.status === "resolved" ? "bg-pink-400" : "bg-blue-200"} border-blue-700	border-[1px] border-dashed`}>
         <CardContent className="flex justify-between items-center p-0 px-1 mt-1">
-          <h3 className="pb-1 flex gap-2"><Cog />{data.name}</h3>
+          <h3 className="pb-1 flex gap-2">{ICONS[data.type]}{data.name}</h3>
           {/* <h3 className="pb-1">{data.type}</h3> */}
           <Sheet >
             <SheetTrigger asChild ><Button className="mb-1">Open</Button></SheetTrigger>
@@ -39,9 +38,9 @@ const Req: React.FC<ReqProps> = ({ data, updateStatus }) => {
                 <SheetTitle>Request info</SheetTitle>
               </SheetHeader>
               <div className="mt-5">
-                <h3 className="font-bold">Name: <span className="font-normal">{data.name}</span></h3>
-                <h3 className="font-bold">Request for:<span className="font-normal">{data.type}</span></h3>
-                <h3 className="font-bold">Desc:</h3>
+                <h3 className="font-bold text-lg text-gray-500">Name: <br/></h3><span className="font-normal">{data.name}</span>
+                <h3 className="font-bold text-lg text-gray-500">Request for:<br/></h3><span className="font-normal">{data.type}</span>
+                <h3 className="font-bold text-lg text-gray-500">Desc:</h3>
                 <p className="border-zinc-500 min-h-10">{data.description}</p>
 
               </div>
